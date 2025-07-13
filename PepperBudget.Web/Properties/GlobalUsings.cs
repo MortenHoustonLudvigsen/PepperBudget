b3613d4333cbbbb94ff866c92cd479902705a30d
Morten Houston Ludvigsen <mlu@2ps.dk>
2025-07-13 11:56:01 +0200

    WIP

--- PepperBudget.Web/Properties/GlobalUsings.cs
+++ PepperBudget.Web/Properties/GlobalUsings.cs
@@ -3,5 +3,6 @@ global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 global using PepperBudget.Data;
 global using PepperBudget.Web;
+global using PepperBudget.Web.Services;
 
 global using System.Net.Http.Json;
